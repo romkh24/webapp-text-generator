71fcf0dc078bf075fe4d3c2ccfc82adc45877c2d
romkh24 <61957283+romkh24@users.noreply.github.com>
2024-03-10 21:59:14 +0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -4,10 +4,10 @@ export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
   title: 'Text Generator APP',
-  description: 'App description',
+  description: 'Hello Test',
   copyright: '',
   privacy_policy: '',
-  default_language: 'zh-Hans',
+  default_language: 'en-US',
 }
 
 export const API_PREFIX = '/api'
